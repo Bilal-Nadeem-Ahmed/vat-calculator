3ad6a6dda935865d93a6f32b3dc77442105fcca7
Bilal-Nadeem-Ahmed <61297097+Megamind44@users.noreply.github.com>
2020-10-09 15:33:48 +0100

    added second and third month
    added the table for the second and third month

--- app.js
+++ app.js
@@ -53,20 +53,22 @@ function createRow(x) {
 
 // container div for rows
 const containerDiv = document.querySelector(".js-html-container");
-//const containerDiv2 = document.querySelector(".js-html-container-2");
-//const containerDiv3 = document.querySelector(".js-html-container-3");
+const containerDiv2 = document.querySelector(".js-html-container-2");
+const containerDiv3 = document.querySelector(".js-html-container-3");
 
 // add the row divs
 let i = 0;
 while (i <= 30) {
   containerDiv.appendChild(createRow("ir"));
-  //containerDiv2.appendChild(createRow("ir2"));
-  //containerDiv3.appendChild(createRow("ir3"));
+  containerDiv2.appendChild(createRow("ir2"));
+  containerDiv3.appendChild(createRow("ir3"));
   i++;
 }
 
 // get rows i made
 const ir = document.querySelectorAll(".ir");
+const ir2 = document.querySelectorAll(".ir2");
+const ir3 = document.querySelectorAll(".ir3");
 
 // function for the input  divs take a for class name, b for inner text
 function makeTheInnerDivs(a) {
@@ -79,11 +81,24 @@ function makeTheInnerDivs(a) {
 //loop through the ir divs and create the input divs
 
 for (let i = 0; i < 31; i++) {
+  // add input divs
   ir[i].appendChild(makeTheInnerDivs("in"));
   // add vat divs
   ir[i].appendChild(makeTheInnerDivs("vat"));
   // add aev divs
   ir[i].appendChild(makeTheInnerDivs("aev"));
+  // add input div
+  ir2[i].appendChild(makeTheInnerDivs("in2"));
+  // add vat divs
+  ir2[i].appendChild(makeTheInnerDivs("vat2"));
+  // add aev divs
+  ir2[i].appendChild(makeTheInnerDivs("aev2"));
+  // add input div
+  ir3[i].appendChild(makeTheInnerDivs("in3"));
+  // add vat divs
+  ir3[i].appendChild(makeTheInnerDivs("vat3"));
+  // add aev divs
+  ir3[i].appendChild(makeTheInnerDivs("aev3"));
 }
 // get the new input divs we made
 const inputDivs = document.querySelectorAll(".in");
@@ -91,6 +106,18 @@ const inputDivs = document.querySelectorAll(".in");
 const vatDivs = document.querySelectorAll(".vat");
 // get the amount excluding vat divs
 const aevDivs = document.querySelectorAll(".aev");
+// get the new input divs we made for month2
+const inputDivs2 = document.querySelectorAll(".in2");
+// get the vat display divs we made for month 2
+const vatDivs2 = document.querySelectorAll(".vat2");
+// get the amount excluding vat divs for month 2
+const aevDivs2 = document.querySelectorAll(".aev2");
+// get the new input divs we made for month3
+const inputDivs3 = document.querySelectorAll(".in3");
+// get the vat display divs we made for month 3
+const vatDivs3 = document.querySelectorAll(".vat3");
+// get the amount excluding vat divs for month 3
+const aevDivs3 = document.querySelectorAll(".aev3");
 // make input field a=classname b is weather input is disabled
 function makeInputField(a, b) {
   let newIf = document.createElement("input");
@@ -109,16 +136,30 @@ for (let i = 0; i < 31; i++) {
   inputDivs[i].appendChild(makeInputField("sn1", false));
   vatDivs[i].appendChild(makeInputField("vat1", true));
   aevDivs[i].appendChild(makeInputField("aev1", true));
+  inputDivs2[i].appendChild(makeInputField("sn20", false));
+  vatDivs2[i].appendChild(makeInputField("vat20", true));
+  aevDivs2[i].appendChild(makeInputField("aev20", true));
+  inputDivs3[i].appendChild(makeInputField("sn30", false));
+  vatDivs3[i].appendChild(makeInputField("vat30", true));
+  aevDivs3[i].appendChild(makeInputField("aev30", true));
 }
-const ayay = document.querySelectorAll(".sn1");
 
 // get the input fields we created
 const aevInput = document.querySelectorAll(".aev1"),
   salesValue1 = document.querySelectorAll(".sn1"),
-  salesVatInput = document.querySelectorAll(".vat1");
+  salesVatInput = document.querySelectorAll(".vat1"),
+  aevInput2 = document.querySelectorAll(".aev20"),
+  salesValue2 = document.querySelectorAll(".sn20"),
+  salesVatInput2 = document.querySelectorAll(".vat20"),
+  aevInput3 = document.querySelectorAll(".aev30"),
+  salesValue3 = document.querySelectorAll(".sn30"),
+  salesVatInput3 = document.querySelectorAll(".vat30");
 
 // total sales value for calc
-let totalSales = 0;
+let totalSales = 0,
+  totalSalesMonth1 = 0,
+  totalSalesMonth2 = 0,
+  totalSalesMonth3 = 0;
 
 // calculate values
 const reducedval = salesValueButton.addEventListener("click", function () {
@@ -126,15 +167,30 @@ const reducedval = salesValueButton.addEventListener("click", function () {
     // update the fields
     aevInput[j].value = (salesValue1[j].value / 120) * 100;
     salesVatInput[j].value = (salesValue1[j].value / 120) * 20;
+    aevInput2[j].value = (salesValue2[j].value / 120) * 100;
+    salesVatInput2[j].value = (salesValue2[j].value / 120) * 20;
+    aevInput3[j].value = (salesValue3[j].value / 120) * 100;
+    salesVatInput3[j].value = (salesValue3[j].value / 120) * 20;
     // calculate the totals
 
     // add the numbers up
     if (Number.isNaN(parseInt(salesValue1[j].value))) {
-      totalSales += 0;
+      totalSalesMonth1 += 0;
     } else {
-      totalSales += parseInt(salesValue1[j].value);
+      totalSalesMonth1 += parseInt(salesValue1[j].value);
+    }
+    if (Number.isNaN(parseInt(salesValue2[j].value))) {
+      totalSalesMonth2 += 0;
+    } else {
+      totalSalesMonth2 += parseInt(salesValue2[j].value);
+    }
+    if (Number.isNaN(parseInt(salesValue3[j].value))) {
+      totalSalesMonth3 += 0;
+    } else {
+      totalSalesMonth3 += parseInt(salesValue3[j].value);
     }
   }
+  totalSales += totalSalesMonth1 + totalSalesMonth2 + totalSalesMonth3;
   setFinalMessage(
     `The total sales are ${totalSales}. The total VAT due on sales is ${parseInt(
       (totalSales / 120) * 20
